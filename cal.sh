110225c277682e0b0657a6096650e85b895db3d7
gateterose <rosegatete5@gmail.com>
2024-03-21 15:55:11 -0700

    question two

--- cal.sh
+++ cal.sh
@@ -22,3 +22,34 @@ echo "Units Consumed: $unitsConsumed"
 echo "Total Bill: Ksh $totalBill"
 #Save the file and make it executable using the following command: chmod +x electricity_bill_calculator.sh
 # Run the script using the following command: ./electricity_bill_calculator.sh
+
+
+
+# Question Two
+echo "Enter Employee Name:"
+read employeeName
+
+echo "Enter Hours Worked:"
+read hoursWorked
+
+echo "Enter Rate Per Hour:"
+read ratePerHour
+basicPay=$(( $hoursWorked * $ratePerHour ))
+if [ $basicPay -gt 70000 ]; then
+    tax=$(( $basicPay * 25 / 100 ))
+elif [ $basicPay -ge 15000 ] && [ $basicPay -le 70000 ]; then
+    tax=$(( $basicPay * 15 / 100 ))
+else
+    tax=0
+fi
+
+netPay=$(( $basicPay - $tax ))
+echo "Employee Name: $employeeName"
+echo "Hours Worked: $hoursWorked"
+echo "Rate Per Hour: $ratePerHour"
+echo "Basic Pay: $basicPay"
+echo "Tax: $tax"
+echo "Net Pay: $netPay"
+
+
+#!/bin/bash
